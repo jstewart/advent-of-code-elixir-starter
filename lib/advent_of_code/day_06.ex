aa6e0499bfbcdf116740822a1916170c563b6308
Jason Stewart <jason@bendyworks.com>
2021-12-07 08:58:00 -0500

    day 6 refactor

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,34 +1,20 @@
 defmodule AdventOfCode.Day06 do
   def part1(args) do
-    process_lanternfish(args, 80)
+    cycle(init_data(args), 80) |> Enum.sum()
   end
 
   def part2(args) do
-    process_lanternfish(args, 256)
+    cycle(init_data(args), 256) |> Enum.sum()
   end
 
-  defp process_lanternfish(fish, iterations) do
-    state =
-      Enum.reduce(0..8, %{}, fn x, acc ->
-        Map.put(acc, x, Enum.count(fish, &(&1 == x)))
-      end)
+  defp init_data(args) do
+    counts = Enum.frequencies(args)
+    Enum.map(0..8, &Map.get(counts, &1, 0))
+  end
 
-    Enum.reduce(1..iterations, state, fn _, acc ->
-      newbs = Map.get(acc, 0)
+  def cycle(l, iteration) when iteration == 0, do: l
 
-      %{
-        8 => newbs,
-        7 => Map.get(acc, 8),
-        6 => Map.get(acc, 7) + newbs,
-        5 => Map.get(acc, 6),
-        4 => Map.get(acc, 5),
-        3 => Map.get(acc, 4),
-        2 => Map.get(acc, 3),
-        1 => Map.get(acc, 2),
-        0 => Map.get(acc, 1)
-      }
-    end)
-    |> Map.values()
-    |> Enum.sum()
+  def cycle([h | t], iteration) do
+    cycle((t ++ [h]) |> List.update_at(6, &(&1 + h)), iteration - 1)
   end
 end
