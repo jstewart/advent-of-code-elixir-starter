b94eab11c3fe914c802ae3b14ec17681f700dbc9
Jason Stewart <jason@bendyworks.com>
2021-12-07 19:25:09 -0500

    d7 p1

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,6 +1,10 @@
 defmodule AdventOfCode.Day07 do
   def part1(args) do
+    target = cheesy_median(args)
+    Enum.map(args, &abs(&1 - target)) |> Enum.sum()
   end
+
+  def cheesy_median(l), do: Enum.sort(l) |> Enum.at(floor(length(l) / 2))
 
   def part2(args) do
   end
