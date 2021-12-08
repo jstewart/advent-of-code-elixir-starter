7e212762c8640b3b69de4c1657c976648d705765
Jason Stewart <jason@bendyworks.com>
2021-12-07 21:16:02 -0500

    day 7  p2

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -4,8 +4,15 @@ defmodule AdventOfCode.Day07 do
     Enum.map(args, &abs(&1 - target)) |> Enum.sum()
   end
 
-  def cheesy_median(l), do: Enum.sort(l) |> Enum.at(floor(length(l) / 2))
+  defp cheesy_median(l), do: Enum.sort(l) |> Enum.at(floor(length(l) / 2))
+  defp cheesy_mean(l), do: Enum.sum(l) / length(l)
+  defp gauss(x), do: x * (x + 1) / 2
 
   def part2(args) do
+    target = cheesy_mean(args)
+
+    c = Enum.map(args, fn x -> gauss(abs(ceil(target) - x)) end) |> Enum.sum()
+    f = Enum.map(args, fn x -> gauss(abs(floor(target) - x)) end) |> Enum.sum()
+    min(c, f) |> trunc
   end
 end
--- lib/mix/tasks/d07.p2.ex
+++ lib/mix/tasks/d07.p2.ex
@@ -5,7 +5,10 @@ defmodule Mix.Tasks.D07.P2 do
 
   @shortdoc "Day 07 Part 2"
   def run(args) do
-    input = nil
+    input =
+      AdventOfCode.Input.get!(7, 2021)
+      |> String.split(",", trim: true)
+      |> Enum.map(&(String.trim(&1) |> String.to_integer()))
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
